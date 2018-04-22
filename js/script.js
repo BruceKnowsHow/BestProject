a76aedc2ee9808a81b03cdf36feb389cf386a867
ttran1214 <tructran0828@gmail.com>
2018-04-22 07:34:24 -0700

    increase font size

--- js/script.js
+++ js/script.js
@@ -5,9 +5,12 @@
 		var sunImage = document.getElementById("sunDisplay").innerHTML =  '<img src="avatar.png">';
 	}
 	
+	function increaseFont(){
+		document.getElementById("paragraph1").style.fontSize = "xx-large";
+	}
+	
 	$(function () {
 	  $(".showMenu").click(function () {
 		$(this).next(".menu").fadeToggle(400);
 	  });
 	});
-		
