580ffb7f85a42868ef8150c5545cf982499206a7
BruceKnowsHow <bruceknowshow@ymail.com>
2018-04-22 14:13:26 -0700

    Expanded map popout

--- js/script.js
+++ js/script.js
@@ -9,17 +9,7 @@
 		document.getElementById("paragraph1").style.fontSize = "xx-large";
 	}
 	
-	
-	function map(){
-					var audio = document.getElementById("song1");
-					
-					setTimeout(alert(""), 2000);
-					setTimeout(song1.play(), 2000);
-				}
-				
-	}
 
-	
 	$(function () {
 	  $(".showMenu").click(function () {
 		$(this).next(".menu").fadeToggle(400);
